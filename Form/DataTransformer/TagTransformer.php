d139039277a47be240ee5e0da7fbb79718e60a0a
Arkadiusz Dzięgiel <admin@glorpen.pl>
2012-02-04 23:19:22 +0100

    added object type guessing for data transformer

--- Form/DataTransformer/TagTransformer.php
+++ Form/DataTransformer/TagTransformer.php
@@ -2,7 +2,6 @@
 namespace Propel\TaggableBehaviorBundle\Form\DataTransformer;
 
 use Symfony\Component\Form\Exception\UnexpectedTypeException;
-
 use Symfony\Component\Form\DataTransformerInterface;
 
 /**
@@ -12,11 +11,16 @@ use Symfony\Component\Form\DataTransformerInterface;
  */
 class TagTransformer implements DataTransformerInterface {
 
+	private $found_class = null;
+	
 	public function transform($tags)
 	{
 		if(empty($tags)) return null;
 		
 		$ret=array();
+		if($tags instanceof \PropelCollection){
+			$this->found_class = $tags->getModel();
+		}
 		if($tags instanceof \PropelCollection || is_array($tags))
 			foreach($tags as $tag)
 				$ret[] = $tag->getName();
@@ -25,6 +29,34 @@ class TagTransformer implements DataTransformerInterface {
 	}
 
     public function reverseTransform($tags){
+    	if($this->found_class!==null){
+    		$cls=$this->found_class.'Query';
+    		
+    		$tags = explode(",",$tags);
+    		
+	    	$ret = $cls::create()
+	    	->filterByName($tags, \Criteria::IN)
+	    	->find();
+	    	
+	    	foreach($tags as $tag){
+	    		$found = false;
+	    		foreach($ret as $obj){
+	    			if($obj->getName() == $tag){
+	    				$found = true;
+	    				break;
+	    			}
+	    		}
+	    		if(!$found){
+	    			$cls = $this->found_class;
+	    			$o = new $cls();
+	    			$o->setName($tag);
+	    			$ret[] = $o;
+	    		}
+	    	}
+	    	
+	    	return $ret;
+    	}
+    	
     	if(empty($tags)) return array();
     	return $tags;
     }
